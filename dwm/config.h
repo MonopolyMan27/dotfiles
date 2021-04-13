7c9a18e6ec1819388507a7b817d8c47f5608cdb4
MonopolyMan27 <81843543+MonopolyMan27@users.noreply.github.com>
2021-04-13 00:43:07 -0700

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -1,8 +1,11 @@
 /* See LICENSE file for copyright and license details. */
 
+ 
 /* appearance */
+
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 10;        /* gap pixel between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "firacode medium:size=13", "FontAwesome5Brands:size=12:antialias:true", "FontAwesome5Free:size=12:antialias:true", "FontAwesome5Free:style=Solid:size=12:antialias:true"};
@@ -19,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "","", ""};
+static const char *tags[] = { "","","","", "","", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
