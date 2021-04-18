05989bad251ffe53bea408680f62628e5f105d20
MonopolyMan27 <81843543+MonopolyMan27@users.noreply.github.com>
2021-04-17 21:55:20 -0700

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -4,8 +4,8 @@
 /* appearance */
 
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 14;        /* gap pixel between windows */  
+static const unsigned int snap      = 15;       /* snap pixel */
+static const unsigned int gappx     = 15;        /* gap pixel between windows */  
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "firacode medium:size=13", "FontAwesome5Brands:size=12:antialias:true", "FontAwesome5Free:size=12:antialias:true", "FontAwesome5Free:style=Solid:size=12:antialias:true"};
@@ -14,7 +14,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#f3a800";
+static const char col_cyan[]        = "#83a598";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -22,7 +22,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "","","","", "","", "", ""};
+static const char *tags[] = { "","","","", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -65,36 +65,38 @@ static const char *discordcmd[]  = { "discord", NULL};
 static const char *bravecmd[] = { "brave", NULL};
 static const char *spotifycmd[] = { "spotify", NULL}; 
 static const char *pcmanfmcmd[] = { "pcmanfm", NULL};
-static const char *slockcmd[] = { "slock", NULL};
-static const char *zoomcmd[] = {"zoom", NULL};
+static const char *firefoxcmd[] = {"firefox", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY|ShiftMask,             XK_y,      spawn,         {.v = dmenucmd} },
+	{ MODKEY|ShiftMask,             XK_j,      spawn,         {.v = dmenucmd} },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,         {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_n,      spawn,         {.v = discordcmd} },
+	{ MODKEY|ShiftMask,             XK_k,      spawn,         {.v = discordcmd} },
 	{ MODKEY|ShiftMask,             XK_b,      spawn,         {.v = bravecmd} },
 	{ MODKEY|ShiftMask,             XK_m,      spawn,         {.v = spotifycmd} },
 	{ MODKEY|ShiftMask,             XK_h,      spawn,         {.v = pcmanfmcmd} },
-	{ MODKEY|ShiftMask,             XK_j,      spawn,         {.v = slockcmd} },
-	{ MODKEY|ShiftMask,             XK_t,      spawn,         {.v = zoomcmd} },
+	{ MODKEY|ShiftMask,             XK_g,      spawn,         {.v = firefoxcmd} },
+	{ MODKEY|ShiftMask,             XK_n,      spawn,         SHCMD("slock") },
 	{ MODKEY|ShiftMask,             XK_v,      spawn,         SHCMD("pavucontrol") },
-	{ MODKEY|ShiftMask,             XK_g,      spawn,         SHCMD("steam") },
-	{ MODKEY|ShiftMask,             XK_i,      spawn,         SHCMD("redshift -O 2700K") },
-	{ MODKEY|ShiftMask,             XK_u,      spawn,         SHCMD("redshift -x") },
-	{ MODKEY|ShiftMask,             XK_k,      spawn,         SHCMD("cd ~/Media/ && scrot -s") },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,         SHCMD("steam") },
+	{ MODKEY|ShiftMask,             XK_u,      spawn,         SHCMD("redshift -O 2700K") },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,         SHCMD("redshift -x") },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,         SHCMD("cd ~/Media/Pictures/ && scrot -s") },
+	{ MODKEY|ShiftMask,             XK_c,      spawn,         SHCMD("mupdf ~/Media/Pictures/pain.png") },
+	{ MODKEY,                       XK_c,      spawn,         SHCMD("setxkbmap -layout us -variant colemak") },
+  { MODKEY,                       XK_q,      spawn,         SHCMD("setxkbmap -layout us ") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_n,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_e,      focusstack,     {.i = -1 } },
+	{ MODKEY,                       XK_u,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_s,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_i,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_w,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
